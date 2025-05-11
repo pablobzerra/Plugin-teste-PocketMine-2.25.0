c06bc63752b7075cee0e98f070cca2076331bb95
Pablo <pabloxgameplay@gmail.com>
2025-05-11 02:28:15 +0000

    update 23:28 10/maio

--- src/testeplugin/Stick.php
+++ src/testeplugin/Stick.php
@@ -4,13 +4,17 @@
 namespace testeplugin;
 
 //imports
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\item\ItemTypeIds;
-use pocketmine\utils\TextFormat;
-use pocketmine\entity\projectile\Snowball;
-use pocketmine\math\Vector3;
+
+use Locale;
 use pocketmine\entity\Location;
+use pocketmine\event\Listener;
+//use pocketmine\event\player\PlayerInteractEvent; <-  click no bloco
+use pocketmine\event\player\PlayerItemUseEvent; // click no ar
+use pocketmine\item\ItemTypeIds;
+//use pocketmine\utils\TextFormat;
+use pocketmine\entity\projectile\Snowball;
+//use pocketmine\math\Vector3;
+//use pocketmine\entity\Location;
 
 class Stick implements Listener {
 
@@ -23,9 +27,13 @@ class Stick implements Listener {
     }
 
 
-    public function onPlayerInteract(PlayerInteractEvent $event) {
+    public function onPlayerInteract(PlayerItemUseEvent $event) {
         $player = $event -> getPlayer(); //estanciando o player
         $item = $player -> getInventory() -> getItemInHand();  //mao do jogador
+        //$action = $event -> getAction(); <- acao !PlayerInteractEvent
+
+        
+        //$this -> plugin -> getServer()-> getLogger() -> info((string)$action);
 
 
         $item_id = $item->getTypeId(); //Id do iten segurado na mao
@@ -37,13 +45,30 @@ class Stick implements Listener {
         if ($item_id === $item_stick) {
             $event -> cancel(); //cancela o uso padrao
             
+            $loc = $player->getLocation(); // <- esta é a forma correta
 
-        
-            $loc = $player -> getLocation();
+            /*
 
-    
+            getLocation() contem |
+                                 v
+            getX(); < -
+            getY(); < -
+            getZ(); < - 
+
+            */
+
+            $x = $loc->getX();
+            $y = $loc->getY() + 5;
+            $z = $loc->getZ();
+
+            $world = $loc->getWorld(); // o mundo que esta 
+            $yaw = $loc->getYaw();  // rotacao horizontal
+            $pitch = $loc->getPitch(); // rotacao vertical
+
+            $i = new Location($x, $y, $z, $world, $yaw, $pitch);
+                
             $snowball = new Snowball(
-            $loc,
+            $i,
             $player
             //,$player->getDirectionVector()->multiply(1.5) // direção e velocidade
         );
